e193bde49f90062133fea291bd4ffdb5af40dffb
yangming <yangming21_ok@163.com>
2020-05-30 17:53:41 +0800

    增加 feign-service日志打印

---
+++ feign-service/src/main/java/com/yangming/cloud/feignservice/config/FeignConfig.java
@@ -0,0 +1,13 @@
+package com.yangming.cloud.feignservice.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
+}
