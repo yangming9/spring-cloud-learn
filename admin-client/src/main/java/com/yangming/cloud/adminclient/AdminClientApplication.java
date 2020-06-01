95c467a09c5824f37690883e67547f0d67f62b76
yangming <yangming21_ok@163.com>
2020-06-01 16:54:41 +0800

    增加 admin-client 模块

--- admin-client/src/main/java/com/yangming/cloud/adminclient/AdminClientApplication.java
+++ admin-client/src/main/java/com/yangming/cloud/adminclient/AdminClientApplication.java
@@ -1,17 +1,18 @@
-package com.yangming.cloud.adminserver;
+package com.yangming.cloud.adminclient;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 使用是发现  admin-server 开启security之后  需要将服务注册到注册中心
+ */
 @EnableDiscoveryClient
-@EnableAdminServer
 @SpringBootApplication
-public class AdminServerApplication {
+public class AdminClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminServerApplication.class, args);
+        SpringApplication.run(AdminClientApplication.class, args);
     }
 
 }
